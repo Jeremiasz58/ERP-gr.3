656081b93605fc7ba40ee5768c09356677919bed
Jeremiasz Mądry <113065500+Jeremiasz58@users.noreply.github.com>
2023-12-03 14:44:26 +0100

    I have no idea what im doinh

---
+++ script/crm_index.js
@@ -0,0 +1,14 @@
+document.getElementById("CRM_c").addEventListener("click", () => {
+    window.location.replace("crm_create.php")
+})
+
+document.getElementById("CRM_r").addEventListener("click", () => {
+    window.location.replace("crm_read.php")
+})
+
+document.getElementById("CRM_u").addEventListener("click", () => {
+    window.location.replace("crm_update.php")
+})
+document.getElementById("back").addEventListener("click", () => {
+    window.location.replace("../main")
+})
